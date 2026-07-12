bc32933dba1835c36c93b094c8e639464d75be90
YashwanthRavulapenta <yashuravulapenta@gmail.com>
2026-07-12 17:29:10 +0530

    middelware

---
+++ middelwares/validationDetails.js
@@ -0,0 +1,28 @@
+
+const validationDetails = (req,res,next) => {
+    const {username , email , subject , message} = req.body
+    if(!username || !email || !subject || !message){
+        return res.send("Feilds are missing...")
+    }
+    if(!username){
+        return res.send("username missing")
+    }
+    if(!email.endsWith("@gmail.com")){
+        return res.send("mail not appropriate")
+    }
+    if(!email){
+        return res.send("email is missing")
+    }
+    if(!subject){
+        return res.send("subject is mising")
+    }
+    if(!message){
+        return res.send("message is not provided")
+    }
+    console.log("yes user details okk")
+    next()
+}
+
+module.exports = {
+    validationDetails
+}
--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const contactController = require('../controllers/contactController')
-
+const validation = require('../middelwares/validationDetails')
 const routerContact = express.Router()
 
-routerContact.post('/contact',contactController.contactControllerLogic)
+routerContact.post('/contact',validation.validationDetails,contactController.contactControllerLogic)
 
 module.exports = routerContact
